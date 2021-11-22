63fdd5f2e0d7043205cb401715d18719d0e242fd
Marc Conrad <marc@sanfoh.com>
2021-11-22 23:13:52 +0000

    html graphic attempt

--- viewloga.php
+++ viewloga.php
@@ -1,117 +1,149 @@
 <!DOCTYPE html>
-<html></html>
+<html>
+
+</html>
+
 <head>
     <title>Log File Analysis</title>
+    <style>
+        td,
+        tr,
+        img {
+            padding: 0px;
+            margin: 0px;
+            border: none;
+            overflow-y: visible;
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+        .outerdiv { 
+            height: 1px; 
+            overflow: visible; 
+        }
+        .innerdiv {
+            height: 150px;
+            overflow: visible;
+            font-size: 20px;
+        }
+    </style>
 </head>
+
 <body>
     <h1>This is still very Beta</h1>
-<?php
-// PHP program to find element closest
-// to given target.
+    <?php
+    $reddot = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAD0lEQVQI1wEEAPv/AMwAAAJoAM1NBPeuAAAAAElFTkSuQmCC";
+    // $ff = @file_get_contents("red1x1.png");
+    // $bb = base64_encode($ff);
+    // $shareicon = '<img height="11em" src="data:image/png;base64,' . $reddot . '" alt="Share this" />';
 
-// Returns element closest to target in arr[]
-function findClosest($arr, $n, $target)
-{
-    // Corner cases
-    if ($target <= $arr[0])
-        return array(-1, 0);
-    // return $arr[0];
-    if ($target >= $arr[$n - 1])
-        return array($n - 1, $n);
-    // return $arr[$n - 1];
+  //   echo '<img height="1pt" width="100pt" src="' . $reddot . '" />';
+    // echo "\r\n";
 
-    // Doing binary search
-    $i = 0;
-    $j = $n;
-    $mid = 0;
-    while ($i < $j) {
-        $mid = (int) (($i + $j) / 2);
+    function findClosest($arr, $n, $target)
+    {
+        // Corner cases
+        if ($target <= $arr[0])
+            return array(-1, 0);
+        // return $arr[0];
+        if ($target >= $arr[$n - 1])
+            return array($n - 1, $n);
+        // return $arr[$n - 1];
 
-        if ($arr[$mid] == $target)
-            return array($mid, $mid);
-        // return $arr[$mid];
+        // Doing binary search
+        $i = 0;
+        $j = $n;
+        $mid = 0;
+        while ($i < $j) {
+            $mid = (int) (($i + $j) / 2);
 
-        /* If target is less than array element,
+            if ($arr[$mid] == $target)
+                return array($mid, $mid);
+            // return $arr[$mid];
+
+            /* If target is less than array element,
             then search in left */
-        if ($target < $arr[$mid]) {
+            if ($target < $arr[$mid]) {
 
-            // If target is greater than previous
-            // to mid, return closest of two
-            if ($mid > 0 && $target > $arr[$mid - 1])
-                return array($mid - 1, $mid);
-            // return getClosest($arr[$mid - 1], $arr[$mid], $target);
+                // If target is greater than previous
+                // to mid, return closest of two
+                if ($mid > 0 && $target > $arr[$mid - 1])
+                    return array($mid - 1, $mid);
+                // return getClosest($arr[$mid - 1], $arr[$mid], $target);
 
-            /* Repeat for left half */
-            $j = $mid;
+                /* Repeat for left half */
+                $j = $mid;
+            }
+
+            // If target is greater than mid
+            else {
+                if (
+                    $mid < $n - 1 &&
+                    $target < $arr[$mid + 1]
+                )
+                    return array($mid, $mid + 1);
+                //  return getClosest($arr[$mid], $arr[$mid + 1], $target);
+                // update i
+                $i = $mid + 1;
+            }
         }
 
-        // If target is greater than mid
-        else {
-            if (
-                $mid < $n - 1 &&
-                $target < $arr[$mid + 1]
-            )
-                return array($mid, $mid + 1);
-            //  return getClosest($arr[$mid], $arr[$mid + 1], $target);
-            // update i
-            $i = $mid + 1;
+        // Only single element left after search
+        return array($mid, $mid);
+        // return $arr[$mid];
+    }
+
+    // Method to compare which one is the more close.
+    // We find the closest by taking the difference
+    // between the target and both values. It assumes
+    // that val2 is greater than val1 and target lies
+    // between these two.
+    function getClosest($val1, $val2, $target)
+    {
+        if ($target - $val1 >= $val2 - $target)
+            return $val2;
+        else
+            return $val1;
+    }
+    $myId = $_GET["id"] ?? 2;
+    $t = file("loga/" . $myId . "_timestamp/__log.html");
+    // var_dump( $t ); 
+    $available_timestamps = array();
+    $timestamps2data = array();
+    foreach ($t as $a) {
+        $parts = explode(" ", $a);
+        $b = substr($a, 0, 15);
+        $w = DateTime::createFromFormat("Ymd-His", $b);
+
+        if ($w === false) {
+            echo "<h2> false for " . $a . " </h2>";
+        } else {
+            $v = $w->getTimestamp();
+
+            $timestamps2data[$v] = $parts;
+            $available_timestamps[] = $v;
         }
     }
 
-    // Only single element left after search
-    return array($mid, $mid);
-    // return $arr[$mid];
-}
+    // var_dump($available_timestamps); 
+    // var_dump($timestamps2data); 
+    $nn = sizeof($available_timestamps);
 
-// Method to compare which one is the more close.
-// We find the closest by taking the difference
-// between the target and both values. It assumes
-// that val2 is greater than val1 and target lies
-// between these two.
-function getClosest($val1, $val2, $target)
-{
-    if ($target - $val1 >= $val2 - $target)
-        return $val2;
-    else
-        return $val1;
-}
-$myId = $_GET["id"] ?? 2; 
-$t = file("loga/".$myId."_timestamp/__log.html");
-// var_dump( $t ); 
-$available_timestamps = array();
-$timestamps2data = array();
-foreach ($t as $a) {
-    $parts = explode(" ", $a);
-    $b = substr($a, 0, 15);
-    $w = DateTime::createFromFormat("Ymd-His", $b);
+    $timeuntil = time();
+    $timestart = time() - 60 * 60 * 24;
+    echo '<table id="maintable">';
+    $previous_data_info = "not yet available";
+    $previous_datapoint = 0;
 
-    if ($w === false) {
-        echo "<h2> false for " . $a . " </h2>";
-    } else {
-        $v = $w->getTimestamp();
+    $gap = 10;
+    $gapthreshold = 40;
+    $zzz = 0; 
+    for ($i = $timeuntil; $i > $timestart; $i = $i - $gap) {
 
-        $timestamps2data[$v] = $parts;
-        $available_timestamps[] = $v;
-    }
-}
 
-// var_dump($available_timestamps); 
-// var_dump($timestamps2data); 
-$nn = sizeof($available_timestamps);
-
-$timeuntil = time();
-$timestart = time() - 60 * 60 * 24;
-echo '<table id="maintable">'; 
-$previous_data_info = "not yet available"; 
-$previous_datapoint = 0; 
-
-$gap = 10; 
-$gapthreshold = 120; 
-for ($i = $timestart; $i < $timeuntil; $i = $i + $gap) {
-   
-
-    $w = findClosest($available_timestamps, $nn, $i);
-    /*
+        $w = findClosest($available_timestamps, $nn, $i);
+        /*
     echo "<h2>" . $i . " = " . date(DATE_RFC2822, $i) . "</h2>";
     
     var_dump($w);
@@ -119,47 +151,61 @@ for ($i = $timestart; $i < $timeuntil; $i = $i + $gap) {
     var_dump($timestamps2data[$available_timestamps[$w[1]]]);
     */
 
-    $data_src_info = "ok";
-    $datapoint = 0;
-    $lower = $available_timestamps[$w[0]] ?? 0;
-    if ($i - $lower > $gapthreshold ) {
-        $lower = false;
-    }
-    $higher = $available_timestamps[$w[1]] ?? PHP_INT_MAX;
-    if ($higher - $i > $gapthreshold ) {
-        $higher = false;
-    }
-
-    if ($higher === false && $lower === false) {
-        $data_src_info = "no data";
-        $datapoint = 0;
-    } else if ($higher === false) {
-        $data_src_info = "lower only";
-        $x = $timestamps2data[$lower];
-        $datapoint = round($x[1]);
-    } else if ($lower === false) {
-        $data_src_info = "higher only";
-        $x = $timestamps2data[$higher];
-        $datapoint = round($x[1]);
-    } else {
         $data_src_info = "ok";
-        $q1 = $timestamps2data[$lower];
-        $q2 = $timestamps2data[$higher];
+        $datapoint = 0;
+        $lower = $available_timestamps[$w[0]] ?? 0;
+        if ($i - $lower > $gapthreshold) {
+            $lower = false;
+        }
+        $higher = $available_timestamps[$w[1]] ?? PHP_INT_MAX;
+        if ($higher - $i > $gapthreshold) {
+            $higher = false;
+        }
 
-        $x1 = round($q1[1]);
-        $x2 = round($q2[1]);
-        $datapoint = round(($x1 + $x2) / 2);
-    }
+        if ($higher === false && $lower === false) {
+            $data_src_info = "no data";
+            $datapoint = 0;
+        } else if ($higher === false) {
+            $data_src_info = "lower only";
+            $x = $timestamps2data[$lower];
+            $datapoint = round($x[1]);
+        } else if ($lower === false) {
+            $data_src_info = "higher only";
+            $x = $timestamps2data[$higher];
+            $datapoint = round($x[1]);
+        } else {
+            $data_src_info = "ok";
+            $q1 = $timestamps2data[$lower];
+            $q2 = $timestamps2data[$higher];
 
-    $xx = abs($datapoint - $previous_datapoint); 
-    
+            $x1 = round($q1[1]);
+            $x2 = round($q2[1]);
+            $datapoint = round(($x1 + $x2) / 2);
+        }
 
-    if($previous_data_info != $data_src_info ) { 
-   // echo "<h5>";
-   echo '<tr class="tablerow" >'; 
-   echo '<td class="tabled" >'; 
-    echo date(DATE_RFC2822, $i);
-     echo '</td>'; 
+        $xx = abs($datapoint - $previous_datapoint);
+
+
+        //  if ($previous_data_info != $data_src_info) {
+        // echo "<h5>";
+        //  echo '<tr height="-100px" class="tablerow" >'; 
+        echo '<tr style="height:1px !important;">';
+        echo '<td class="tabled" style="font-size: 1px;" >&nbsp;';
+        echo '<img height="1px" alt="hello alt" title="'.date(DATE_RFC2822, $i).'; ' .$datapoint.'; ' .$data_src_info.'" width="' . (3 * $datapoint) . 'px" src="' . $reddot . '" />';
+        echo '</td><td><div class="outerdiv">'; 
+        $zzz++; 
+        if ($zzz % 20 == 2) {
+        echo '<div class="innerdiv">' . date(DATE_RFC2822, $i) . '</div>';
+        }
+        echo '</div>';
+        echo '</td>';
+        echo '</td><td><div class="outerdiv">'; 
+        if ($zzz % 20 == 2) {
+        echo '<div class="innerdiv">&nbsp; &nbsp; ' . $datapoint . '</div>';
+        }
+        echo '</div>';
+        echo '</td>';
+        /*
     echo '<td class="tabled" >';
     echo " data=" . $datapoint;
     echo '</td>'; 
@@ -167,14 +213,17 @@ for ($i = $timestart; $i < $timeuntil; $i = $i + $gap) {
     echo " src=" . $data_src_info;
    //  echo "</h5>";
     echo '</td>'; 
-    echo '</tr>';
-    echo "\r\n"; 
-    }
 
-    $previous_data_info = $data_src_info; 
-    $previous_datapoint = $datapoint; 
-}
-echo '</table>'; 
-?>
+    */
+        echo '</tr>';
+        // echo "\r\n"; 
+        //   }
+
+        $previous_data_info = $data_src_info;
+        $previous_datapoint = $datapoint;
+    }
+    echo '</table>';
+    ?>
 </body>
+
 </html>
